7283f463a4c371a813fed21aaa8ec115e437d848
Philip Morrell <ubuntu.emorrp1@mamber.net>
2009-06-21 17:02:21 +0100

    properly setting up populate

--- populate.py
+++ populate.py
@@ -5,7 +5,9 @@ from datetime import datetime
 
 metadata.bind = "sqlite:///custom.hike" # connect
 
-home = Location(e=000,n=000,id=0)
+setup_all()
+
+home = Location(e=000,n=000)
 noon = datetime(2009,06,22,12,00)
 home_noon = Event(time=noon,loc=home)
 team = Team(number=101)
