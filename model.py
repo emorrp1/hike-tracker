e02230a78bb7c46ab8207c390dad578e933b18ad
Philip Morrell <ubuntu.emorrp1@mamber.net>
2009-06-21 15:59:25 +0100

    removing extra location info

--- model.py
+++ model.py
@@ -1,10 +1,8 @@
 from elixir import *
 
 class Location(Entity):
-  using_options(inheritance='multi')
   e = Field(Integer)
   n = Field(Integer)
-  t = Field(Integer) #minutes since set time
 
   def __repr__(self):
     return '<Location %s %s>' % (self.x-coord, self.y-coord)
