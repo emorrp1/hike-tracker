f4d38d37c51ec9135576fd73a5dad38d8aa53fe7
Philip Morrell <ubuntu.emorrp1@mamber.net>
2009-06-21 15:04:44 +0100

    removing persons

--- model.py
+++ model.py
@@ -16,25 +16,9 @@ class Base(Location):
   def __repr__(self):
     return '<Base "%s">' % self.name
 
-class Person(Entity):
-  using_options(inheritance='multi')
-  name = Field(Unicode(60))
-
-  def __repr__(self):
-    return '<Person "%s">' % self.name
-
-class Participant(Person):
-  using_options(inheritance='multi')
-
-  team = ManyToOne('Team')
-
-  def __repr__(self):
-    return '<Participant "%s">' % self.name
-
 class Team(Entity):
   number = Field(Integer)
 
-  participants = OneToMany('Participant')
   location = ManyToOne('Location')
 
   def __repr__(self):
