29790bae09a5daf326951e1a6c4c80d0ea890572
Philip Morrell <ubuntu.emorrp1@mamber.net>
2009-06-21 16:36:13 +0100

    updating populate to new simple_model

--- populate.py
+++ populate.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 metadata.bind = "sqlite:///custom.hike" # connect
 
-home = Location(e=000,n=000)
+home = Location(e=000,n=000,id=0)
 noon = datetime(2009,06,22,12,00)
 home_noon = Event(time=noon,loc=home)
 team = Team(number=101)
