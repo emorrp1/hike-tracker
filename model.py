29a4c4f2357bfc2bae9ca22ed3a4ef2f65559e0a
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-07-21 12:49:01 +0100

    adding Base.report helper function

--- model.py
+++ model.py
@@ -1,10 +1,18 @@
 from elixir import *
+from datetime import datetime
+DATE='090721'
 
 class Base(Entity):
 	id = Field(Integer)
 	e = Field(Integer)
 	n = Field(Integer)
 	reports = OneToMany('Report')
+
+	def report(self, team_id, arr, dep, date=DATE)
+		arr = datetime(int(date[:2]),int(date[2:-2]),int(date[-2:]),int(arr[:2]),int(arr[-2:]))
+		dep = datetime(int(date[:2]),int(date[2:-2]),int(date[-2:]),int(dep[:2]),int(dep[-2:]))
+		team = Team.get_by(id=team_id)
+		reports.append(Report(arr=
 
 	def __repr__(self):
 		return '<Base %s>' % self.id
