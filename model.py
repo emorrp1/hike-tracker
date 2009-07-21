775105a91f9cffe5cd309aafa6d7fe1a268d7cf1
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-07-21 11:57:04 +0100

    re-adding Base

--- model.py
+++ model.py
@@ -1,11 +1,19 @@
 from elixir import *
 
 class Location(Entity):
+	using_options(inheritance='multi')
 	e = Field(Integer)
 	n = Field(Integer)
 
 	def __repr__(self):
 		return '<Location %sE %sN>' % (self.e, self.n)
+
+class Base(Location):
+	using_options(inheritance='multi')
+	name = Field(Unicode(1))
+
+	def __repr__(self):
+		return '<Base %s>' % self.name
 
 class Event(Entity):
 	loc = ManyToOne('Location')
