f9219b684421a047bdab54343096113e23acb509
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-07-21 13:07:35 +0100

    finishing converting populate to new model

--- populate.py
+++ populate.py
@@ -19,49 +19,22 @@ t4 = Team(number=104)
 
 session.commit()
 
-b0.report(101, '12:00', '12:00')
-b0.report(102, '12:00', '12:00')
-b0.report(103, '12:00', '12:00')
-b0.report(104, '12:00', '12:00')
+b0.report(101, '12:00')
+b0.report(102, '12:00')
+b0.report(103, '12:00')
+b0.report(104, '12:00')
 
-team_1.seen.append(br_noon_15)
-team_2.seen.append(br_noon_45)
-team_3.seen.append(br_noon_45)
-team_4.seen.append(br_noon_45)
+b1.report(101, '12:15')
+b1.report(102, '12:45')
+b1.report(103, '12:45')
+b1.report(104, '12:45')
 
-team_1.seen.append(tl_noon_45)
+b2.report(102, '12:30')
+b2.report(103, '12:15')
+b2.report(104, '12:15', '12:30')
 
-team_2.seen.append(tl_noon_15)
-team_2.seen.append(tr_noon_30)
+b3.report(101, '12:45')
+b3.report(102, '12:15')
+b3.report(103, '12:30')
 
-team_3.seen.append(tr_noon_15)
-team_3.seen.append(tl_noon_30)
-
-team_4.seen.append(tr_noon_15)
-team_4.seen.append(tr_noon_30)
-
-noon = datetime(2009,06,22,12,00)
-noon_15 = datetime(2009,06,22,12,15)
-noon_30 = datetime(2009,06,22,12,30)
-noon_45 = datetime(2009,06,22,12,45)
-
-br_noon = Event(time=noon,loc=br)
-tr_noon = Event(time=noon,loc=tr)
-tl_noon = Event(time=noon,loc=tl)
-bl_noon = Event(time=noon,loc=bl)
-
-br_noon_15 = Event(time=noon_15,loc=br)
-tr_noon_15 = Event(time=noon_15,loc=tr)
-tl_noon_15 = Event(time=noon_15,loc=tl)
-bl_noon_15 = Event(time=noon_15,loc=bl)
-
-br_noon_30 = Event(time=noon_30,loc=br)
-tr_noon_30 = Event(time=noon_30,loc=tr)
-tl_noon_30 = Event(time=noon_30,loc=tl)
-bl_noon_30 = Event(time=noon_30,loc=bl)
-
-br_noon_45 = Event(time=noon_45,loc=br)
-tr_noon_45 = Event(time=noon_45,loc=tr)
-tl_noon_45 = Event(time=noon_45,loc=tl)
-bl_noon_45 = Event(time=noon_45,loc=bl)
-
+session.commit()
