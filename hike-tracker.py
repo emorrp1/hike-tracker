125005173f542aaa87e61f68639a85a40f3e72a3
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-07-29 10:30:59 +0100

    moving init to fancy start func

---
+++ hike-tracker.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import os
+from elixir import *
+from model import *
+
+def start(filename="custom.hike"):
+	metadata.bind = "sqlite:///" + filename # connect
+	if os.path.exists(os.path.abspath(filename)):
+		setup_all()
+	else:
+		options_defaults['tablename'] = lambda c: c.__name__.lower()
+		setup_all(True)
+
