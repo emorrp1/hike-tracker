55949daac8402e1ab87511df5c64edb800b0c1b7
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-07-30 00:18:01 +0100

    tidying start

--- tracker.py
+++ tracker.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
-import os
 from elixir import *
 
 def start(filename="custom.hike"):
+	from os.path import exists
 	metadata.bind = "sqlite:///" + filename # connect
-	options_defaults['tablename'] = lambda c: c.__name__.lower()
 	setup_all()
-	if not os.path.exists(filename):
+	if not exists(filename):
 		create_all()
 
 save = session.commit
