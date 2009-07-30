7ab44d3ec872eb5a1c646cd3809d823be02e99a9
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-07-30 15:50:35 +0100

    updating Team.finished

--- tracker.py
+++ tracker.py
@@ -117,8 +117,7 @@ class Team(Entity):
 		return self.last_visited() in self.route.bases
 
 	def finished(self):
-		last = len(self.route.bases) - 1
-		return self.last_visited() == self.route.bases[last]
+		return self.last_visited() == self.route.end
 
 	def completed(self):
 		for base in self.route.bases:
