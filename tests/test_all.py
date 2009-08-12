c53b8d1a39b8c80a3e77847b70df0d8cc2d811fc
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-08-12 10:37:42 +0100

    adding tracker test suite

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,11 +3,19 @@ from tracker import start
 import unittest
 import test_base, test_route, test_team
 
+class TestTracker(unittest.TestCase):
+	def setUp(self):
+		start('test')
+
+	def tearDown(self):
+		elixir.session.close()
+
 def suite():
+	tracker_suite = unittest.TestLoader().loadTestsFromTestCase(TestTracker)
 	base_suite = test_base.suite()
 	route_suite = test_route.suite()
 	team_suite = test_team.suite()
-	return unittest.TestSuite([base_suite, route_suite, team_suite])
+	return unittest.TestSuite([tracker_suite, base_suite, route_suite, team_suite])
 
 if __name__ == '__main__':
 	start('test')
