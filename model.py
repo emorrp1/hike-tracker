b8362f98f324995e46023e375062255fc28ffb71
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-08-17 11:32:35 +0100

    adding model.config with start

--- model.py
+++ model.py
@@ -322,3 +322,6 @@ def mkdt(time, date=None):
 		time = datetime.strptime(time,'%H:%M').time()
 	return datetime.combine(date, time)
 START = mkdt('08:00', datetime.today().date())
+config = {
+		'start':mkdt('08:00', datetime.today().date())
+		}
