0241a34ebd516bb92858beee1e4f8c4b4e92aba9
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-08-20 21:55:31 +0100

    making Team.late use isinstance check

--- model.py
+++ model.py
@@ -246,7 +246,7 @@ class Team(Entity):
 	def late(self, leeway=0, speed=None, base=None):
 		eta = self.eta(base, speed)
 		if eta:
-			if type(leeway).__name__ in ['int', 'str']:
+			if isinstance(leeway, (int, str)):
 				leeway = timedelta(minutes=int(leeway))
 			return eta + leeway < datetime.now()
 		else:
