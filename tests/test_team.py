75088503dc945d950327a6bc0a2e64b1afa7107b
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-08-23 10:48:19 +0100

    fixing TestTeam.testTraversed to use model.Config

--- tests/test_team.py
+++ tests/test_team.py
@@ -64,7 +64,7 @@ class TestTeam(unittest.TestCase):
 		self.assertEqual(empty, t.last_visited())
 
 	def testTraversed(self):
-		self.assertEqual(model.config['wfact'], 1.3)
+		self.assertEqual(model.Config['wfact'], 1.3)
 		t = model.Team('test')
 		self.assertEqual(t.traversed(), 0)
 		self.assertTrue(isinstance(get('t1').traversed(), int))
