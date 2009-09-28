388762da488eb1cc236e610724847685fabae409
Philip Morrell <hike-tracker.emorrp1@mamber.net>
2009-09-28 09:37:07 +0100

    moving next tests to TestRoute

--- tests/test_route.py
+++ tests/test_route.py
@@ -20,6 +20,17 @@ class TestRoute(unittest.TestCase):
 		length = r.bases[0].distgain_along(r, r.end())['dist']
 		self.assertEqual(length, len(r))
 
+	def testNext1(self):
+		self.assertEqual(get('b0').next('1'), get('b1'))
+		self.assertEqual(get('b1').next('1'), get('b3'))
+		self.assertEqual(get('b3').next('1'), get('b2'))
+		self.assertEqual(get('b2').next('1'), None)
+
+	def testNext2(self):
+		self.assertEqual(get('b0').next('2'), get('b2'))
+		self.assertEqual(get('b2').next('2'), get('b3'))
+		self.assertFalse(get('b3').next('2'))
+
 def suite():
 	return unittest.TestLoader().loadTestsFromTestCase(TestRoute)
 
